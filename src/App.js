d3856f2f4e93ebb2673c365439a389fd03651d66
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2017-12-16 15:55:17 +0800

    props and children

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello World! React</h1>
+        <Person name="Andy" age="21"/>
+        <Person name="Jack" age="22">Hobby is coding</Person>
       </div>
+
     );
   }
 }
